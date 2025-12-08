68e749134d02e6d5b5386262bb704dc50fb607dc
lukin921 <lukin921@student.liu.se>
2025-12-08 11:33:36 +0100

    copilot plugin update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,15 +5,6 @@ return {
       { 'nvim-lua/plenary.nvim', branch = 'master' },
     },
     build = 'make tiktoken',
-    opts = {
-      model = 'gpt-4.1',
-      temperature = 0.1, -- Lower = focused, higher = creative
-      window = {
-        layout = 'vertical', -- 'vertical', 'horizontal', 'float'
-        width = 0.5, -- 50% of screen width
-      },
-      auto_insert_mode = true,
-    },
     keys = {
       {
         '<leader>cc',
