bc4824aea416dba673ad002b33c496dd196ee945
lukin921 <lukin921@student.liu.se>
2026-01-14 20:14:32 +0100

    changed nvim init file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,10 @@
 -- Set leader keys FIRST
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = false
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
 
 -- Load core configuration
-require 'options'
-require 'keymaps'
-require 'lazy_bootstrap'
-require 'lazy_plugins'
+require("options")
+require("keymaps")
+require("lazy_bootstrap")
+require("lazy_plugins")
